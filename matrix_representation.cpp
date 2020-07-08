1d7256a6fbd673849036b8235b8b19197c651478
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2020-07-08 21:56:31 +0200

    A matrix representation.

--- matrix_representation.cpp
+++ matrix_representation.cpp
@@ -1,10 +1,11 @@
 //Author : Michał Traczyk
+
 #include <bits/stdc++.h>
 
 typedef long long ll;
-const int M=1e6+7;
-const ll q=7057594037927903;
-const ll prime=2137;
+const int M = 1e6 + 7;
+const ll q = 7057594037927903;
+const ll prime = 2137;
 #define pb push_back
 #define mp make_pair
 #define f first
@@ -12,59 +13,56 @@ const ll prime=2137;
 #define ios ios_base::sync_with_stdio(0)
 
 struct matrix {
-    int **tab;
-    int numberOfRows;
-    int numberOfColumns;
+    ll **tab;
+    ll numberOfRows;
+    ll numberOfColumns;
 
     matrix(int n, int m) {
         numberOfRows = n;
         numberOfColumns = m;
-        tab = new int*[n];
-        for(int i = 0; i < n; i++) {
-            tab[i] = new int[m];
-            for(int j = 0; j < m; j++) {
+        tab = new ll *[n];
+        for (int i = 0; i < n; i++) {
+            tab[i] = new ll[m];
+            for (int j = 0; j < m; j++) {
                 tab[i][j] = 0;
             }
         }
     }
 
     void deleteMatrix() {
-        for(int i = 0; i < numberOfRows; i++) {
+        for (int i = 0; i < numberOfRows; i++) {
             delete[] tab[i];
         }
         delete[] tab;
     }
 
     void identityMatrix() {
-        for(int i = numberOfRows - 1; i >= 0; i--) {
-            tab[i][numberOfRows - 1 - i] = 1;
+        for (int i = numberOfRows - 1; i >= 0; i--) {
+            tab[i][numberOfRows - 1 - i] = 1LL;
         }
     }
 
     void printMatrix() {
-        for(int i = numberOfRows - 1; i >= 0; i--) {
-            for(int j = 0; j < numberOfColumns; j++) {
-                printf("%d ", tab[i][j]);
+        for (int i = numberOfRows - 1; i >= 0; i--) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                printf("%lld ", tab[i][j]);
             }
             printf("\n");
         }
     }
 };
 
+//If you want it to work for non square matrices, you have to allocate a required memory.
 inline matrix operator*(const matrix &a, const matrix &b) {
     matrix c = matrix(a.numberOfRows, b.numberOfColumns);
 
-    for(int i = 0; i < c.numberOfRows; i++) {
-        for(int j = 0; j < c.numberOfColumns; j++) {
-            for(int k = 0; k < a.numberOfColumns; k++) {
+    for (int i = 0; i < c.numberOfRows; i++) {
+        for (int j = 0; j < c.numberOfColumns; j++) {
+            for (int k = 0; k < a.numberOfColumns; k++) {
                 c.tab[i][j] += a.tab[i][k] * b.tab[b.numberOfRows - 1 - k][j];
             }
         }
     }
 
     return c;
-}
-
-int main() {
-    return 0;
 }
