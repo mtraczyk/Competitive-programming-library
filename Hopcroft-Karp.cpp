ed0499df0e5cfd716a871e44aa7548bde153de91
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2020-08-01 11:10:40 +0200

    An implementation of Hopcroft-Karp algorithm.

--- Hopcroft-Karp.cpp
+++ Hopcroft-Karp.cpp
@@ -1,160 +1,104 @@
+//Author : Michał Traczyk
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long ll;
-const int M=100007;
-const ll q=7057594037927903;
-const ll prime=2137;
+const int M = 1000007;
+const ll q = 7057594037927903;
+const ll prime = 2137;
 #define pb push_back
 #define mp make_pair
 #define f first
 #define s second
 
-int dis[M],vis[M],match[M],n,m,odwiedzone[M],kolor[M],odwiedzone2[M]; ll tab[M];
-queue <int>Q; vector <int>v[M],ans; map <ll,int>zbior;
+int match[M], n, dis[M]; //n - number of vertices
+bool visited[M], color[M];
+vector<int> v[M];
+queue<int> Q;
 
-int dfs(int h)
-{
-    if(vis[h])
+int dfs(int h) {
+    if (visited[h]) {
         return 0;
-    vis[h]=true;
+    }
+    visited[h] = true;
 
-    for(int i=0; i<(int)v[h].size(); i++)
-    {
-        if(!match[v[h][i]])
-        {
-            match[h]=v[h][i];
-            match[v[h][i]]=h;
+    for (int i = 0; i < (int) v[h].size(); i++) {
+        if (!match[v[h][i]]) {
+            match[h] = v[h][i];
+            match[v[h][i]] = h;
             return 1;
-        }
-        else if(dis[match[v[h][i]]]==dis[h]+1 && dfs(match[v[h][i]]))
-        {
-            match[h]=v[h][i];
-            match[v[h][i]]=h;
+        } else if ((dis[match[v[h][i]]] == dis[h] + 1) && dfs(match[v[h][i]])) {
+            match[h] = v[h][i];
+            match[v[h][i]] = h;
             return 1;
         }
     }
     return 0;
 }
 
-void matching()
-{
-    int res=0,pres=-1;
-    while(res!=pres)
-    {
-        pres=res;
-        for(int i=1; i<=n; i++)
-            dis[i]=0,vis[i]=0;
+void find_maximal_matching() {
+    int res = 0, pres = -1;
+    while (res != pres) {
+        pres = res;
+        for (int i = 1; i <= n; i++) {
+            visited[i] = 0;
+        }
 
-        /*  for(int i=1; i<=n; i++)
-              if(!match[i] && !kolor[i])
-                  Q.push(i),dis[i]=0,vis[i]=1;
+        for (int i = 1; i <= n; i++)
+            if (!match[i] && !color[i]) {
+                Q.push(i), dis[i] = 0, visited[i] = 1;
+            }
 
 
-          while(!Q.empty())
-          {
-              int w=Q.front();
-              Q.pop();
-              for(int i=0; i<(int)v[w].size(); i++)
-              {
-                  if(match[v[w][i]] && !vis[match[v[w][i]]])
-                  {
-                      int pom=match[v[w][i]];
-                      Q.push(pom);
-                      dis[pom]=dis[w]+1;
-                      vis[pom]=1;
-                  }
-              }
-          }
+        while (!Q.empty()) {
+            int w = Q.front();
+            Q.pop();
+            for (int i = 0; i < (int) v[w].size(); i++) {
+                if (match[v[w][i]] && !visited[match[v[w][i]]]) {
+                    int aux = match[v[w][i]];
+                    Q.push(aux);
+                    dis[aux] = dis[w] + 1;
+                    visited[aux] = 1;
+                }
+            }
+        }
 
-          for(int i=1; i<=n; i++)
-              vis[i]=0;
-        */
+        for (int i = 1; i <= n; i++) {
+            visited[i] = 0;
+        }
 
-        for(int i=1; i<=n; i++)
-            if(!match[i] && !kolor[i])
-                res+=dfs(i);
+        for (int i = 1; i <= n; i++)
+            if (!match[i] && !color[i]) {
+                res += dfs(i);
+            }
     }
 }
 
-void wczytaj()
-{
-    scanf("%d",&n);
-    for(int i=1; i<=n; i++)
-    {
-        scanf("%lld",&tab[i]);
-        zbior[tab[i]]=i;
-    }
-
-    for(int i=1; i<=n; i++)
-    {
-        if(zbior[2LL*tab[i]])
-            v[i].pb(zbior[2LL*tab[i]]);
-
-        if(zbior[3LL*tab[i]])
-            v[i].pb(zbior[3LL*tab[i]]);
-
-        if(tab[i]%2LL==0 && zbior[tab[i]/2LL])
-            v[i].pb(zbior[tab[i]/2LL]);
-
-        if(tab[i]%3LL==0 && zbior[tab[i]/3LL])
-            v[i].pb(zbior[tab[i]/3LL]);
-    }
-}
-
-void dfs2(int h)
-{
-    odwiedzone[h]=1;
-    for(int i=0; i<(int)v[h].size(); i++)
-        if(!odwiedzone[v[h][i]])
-        {
-            if(kolor[h]==0)
-                kolor[v[h][i]]=1;
-            else
-                kolor[v[h][i]]=0;
-            dfs2(v[h][i]);
+void color_the_graph(int h) {
+    visited[h] = 1;
+    for (int i = 0; i < (int) v[h].size(); i++)
+        if (!visited[v[h][i]]) {
+            if (color[h] == 0) {
+                color[v[h][i]] = 1;
+            } else {
+                color[v[h][i]] = 0;
+            }
+            color_the_graph(v[h][i]);
         }
 }
 
-void dfs3(int h)
-{
-    odwiedzone2[h]=1;
-    for(int i=0; i<(int)v[h].size(); i++)
-    {
-        if(!kolor[h] && v[h][i]!=match[h] && !odwiedzone2[v[h][i]])
-            dfs3(v[h][i]);
-        else if(kolor[h] && v[h][i]==match[h] && !odwiedzone2[v[h][i]])
-            dfs3(v[h][i]);
-    }
+void find_coloring() {
+    for (int i = 1; i <= n; i++)
+        if (!visited[i]) {
+            color_the_graph(i);
+        }
 }
 
-int main()
-{
-    wczytaj();
-    for(int i=1; i<=n; i++)
-        if(!odwiedzone[i])
-            dfs2(i);
+int main() {
+    find_coloring();
+    find_maximal_matching();
 
-    matching();
-
-    int wynik=n;
-    for(int i=1; i<=n; i++)
-        if(!kolor[i] && match[i])
-            wynik--;
-
-    for(int i=1; i<=n; i++)
-        if(!odwiedzone2[i] && !match[i] && !kolor[i])
-            dfs3(i);
-
-    for(int i=1; i<=n; i++)
-        if((!kolor[i] && odwiedzone2[i] && match[i]) || (kolor[i] && !odwiedzone2[i] && match[i]) || !match[i])
-            ans.pb(i);
-
-    printf("%d\n",wynik);
-    for(int i=0; i<(int)ans.size(); i++)
-        printf("%lld ",tab[ans[i]]);
-
-    printf("\n");
     return 0;
 }
